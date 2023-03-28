30f1b7724d7eb836da77d43f38a5d791a645d26e
JoseR64 <jramon.dh@gmail.com>
2023-03-28 20:15:51 +0100

    Proyecto cambios a las 20:15 clase string

---
+++ src/com/hedima/presentacion/OperadoresLogicos.java
@@ -0,0 +1,30 @@
+package com.hedima.presentacion;
+
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        boolean resultado = a >= b;
+        System.out.println(resultado);
+        resultado = a <= b;
+        System.out.println(resultado);
+
+        int c = 10;
+        int d = 10;
+        resultado = c == d;
+        System.out.println(resultado);
+
+        resultado = c!= d;
+        System.out.println(resultado);
+
+        //Operadores logicos
+        resultado = (a > b) && (a < c);
+        System.out.println("ND->"+resultado);
+        resultado = (a < b) || (a > c);
+        System.out.println(resultado);
+        resultado = !(a >= b);
+        System.out.println(resultado);
+
+
+    }
+}
